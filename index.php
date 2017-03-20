7a58cb2d39b2d46574e252cb292eb4a62028a2ed
Douglas Maia <douglasmaiabr@gmail.com>
2017-03-19 23:45:13 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,786 @@
+﻿
+
+<?php header ('Content-type: text/html; charset=ISO-8859-1'); ?>
+
+<!DOCTYPE html>
+
+<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
+
+<html>
+
+<head>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-93977848-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,700,700italic,800,800italic,300italic,300" rel="stylesheet">
+
+<meta charset="utf-8">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Vamos te ajudar a vender na internet, como nenhum outro blog já ensinou!</title>
+
+<meta name="description" content="Aprenda a vender na internet de graça, e de forma gratuita! Acredite, somos bons nisso!">
+
+
+
+<!--Favicon-->
+
+
+
+<link rel="apple-touch-icon" sizes="57x57" href="img/favicons/apple-touch-icon-57x57.png">
+
+<link rel="apple-touch-icon" sizes="60x60" href="img/favicons/apple-touch-icon-60x60.png">
+
+<link rel="apple-touch-icon" sizes="72x72" href="img/favicons/apple-touch-icon-72x72.png">
+
+<link rel="apple-touch-icon" sizes="76x76" href="img/favicons/apple-touch-icon-76x76.png">
+
+<link rel="apple-touch-icon" sizes="114x114" href="img/favicons/apple-touch-icon-114x114.png">
+
+<link rel="apple-touch-icon" sizes="120x120" href="img/favicons/apple-touch-icon-120x120.png">
+
+<link rel="apple-touch-icon" sizes="144x144" href="img/favicons/apple-touch-icon-144x144.png">
+
+<link rel="apple-touch-icon" sizes="152x152" href="img/favicons/apple-touch-icon-152x152.png">
+
+<link rel="apple-touch-icon" sizes="180x180" href="img/favicons/apple-touch-icon-180x180.png">
+
+<link rel="icon" type="image/png" href="img/favicons/favicon-32x32.png" sizes="32x32">
+
+<link rel="icon" type="image/png" href="img/favicons/android-chrome-192x192.png" sizes="192x192">
+
+<link rel="icon" type="image/png" href="img/favicons/favicon-96x96.png" sizes="96x96">
+
+<link rel="icon" type="image/png" href="img/favicons/favicon-16x16.png" sizes="16x16">
+
+<link rel="manifest" href="img/favicons/manifest.json">
+
+<link rel="shortcut icon" href="img/favicons/favicon.ico">
+
+<meta name="msapplication-TileColor" content="#2b5797">
+
+<meta name="msapplication-TileImage" content="img/favicons/mstile-144x144.png">
+
+<meta name="msapplication-config" content="img/favicons/browserconfig.xml">
+
+<meta name="theme-color" content="#ffffff">
+
+
+
+<!--Favicon end-->
+
+
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<link href="css/font-awesome.min.css" rel="stylesheet">
+
+<link href="css/magnific-popup.css" rel="stylesheet">
+
+<link href="css/owl.carousel.css" rel="stylesheet">
+
+<link href="css/main.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+
+
+<!--hero section-->
+
+
+
+<header class="hero-section">
+
+
+
+  <!--navigation-->
+
+
+
+  <nav class="navbar navbar-default" data-spy="affix" data-offset-top="450">
+
+    <div class="container">
+
+      <div class="navbar-header"> <a class="navbar-brand" href=""><img class="logo" alt="logo" src="img/logo.png"><img class="logo-nav" alt="logo-nav" src="img/logo-nav.png" style="margin-top: -34px;"></a> </div>
+
+      <ul class="nav navbar-nav navbar-right">
+
+        <li class="hidden-xs hidden-sm"><a href="#features">Sobre nós</a></li>
+
+
+        <li class="hidden-xs hidden-sm"><a href="#reviews">Comentários</a></li>
+
+        <li class="hidden-xs"><a data-toggle="modal" data-target="#modal-contact-form">Aprenda a Vender!</a></li>
+
+
+
+      </ul>
+
+    </div>
+
+  </nav>
+
+
+
+  <!--navigation end-->
+
+
+
+  <!--mobile navigation-->
+
+  <!--
+
+  <div class="mobile-nav-overlay hidden-md hidden-lg" id="mobile-nav-overlay">
+
+    <nav class="mobile-nav">
+
+      <ul>
+
+        <li><a href="#features">Features</a></li>
+
+        <li><a href="#pricing">Pricing</a></li>
+
+        <li><a href="#reviews">Reviews</a></li>
+
+        <li><a href="#faq">FAQ</a></li>
+
+        <li><a href="#team">Team</a></li>
+
+        <li class="hidden-sm"><a href="signin.html">Sign In</a></li>
+
+      </ul>
+
+    </nav>
+
+  </div>
+
+  -->
+
+  <!--mobile navigation end-->
+
+
+
+  <!--welcome message-->
+
+
+<section class="container text-center welcome-message">
+
+    <div class="row">
+
+      <div class="col-md-11 center-block">
+
+        <div class="row">
+
+          <div class="col-md-6">
+
+            <h1>Você quer vender muito pela internet?</h1>
+
+            <h2>Nós <b> vamos te ajudar </b>, e o melhor, sem cobrar absolutamente nada por isso!</h2>
+
+
+
+          </div>
+
+
+
+          <!--cta signup form-->
+
+
+
+          <div class="col-md-6">
+
+            <section class="cta-form cta-light">
+
+              <form id="cta-signup-form" class="cta-signup-form" method="post" action="cadastro.php">
+
+                <h2 class="text-center">Preencha abaixo e receba AGORA <br> um e-book com um conteúdo rápido e simples sobre vendas na internet.<br><span style="
+    font-size: 13px;
+    color: #929292;
+    font-style: italic;
+    font-weight: lighter;
+"> Além disso você terá acesso a todo o nosso conteúdo gratuitamente.</span> </h2>
+
+                <div id="mc-error-cta"><i class="fa fa-exclamation-triangle"></i> Please enter a value</div>
+
+                <div id="mc-success-cta"><i class="fa fa-envelope"></i> Awesome! We have sent you a confirmation email</div>
+
+                <div class="form-group"> <i class="fa fa-user"></i>
+
+                  <input type="text" value="" name="nome" class="form-control input-lg" id="input-name" placeholder="Seu Nome" required="">
+
+                </div>
+
+                <div class="form-group"> <i class="fa fa-envelope-o"></i>
+
+                  <input type="email" value="" name="email" class="form-control input-lg" id="input-email" placeholder="Seu E-mail" required="">
+
+                </div>
+
+
+
+                <div class="form-btn">
+
+                  <button type="submit" class="btn">Eu quero vender pela internet!</button>
+
+                  <p class="form-terms">Nós nos responsabilizamos em não enviar spam em nenhum momento.</p>
+
+                </div>
+
+              </form>
+
+            </section>
+
+          </div>
+
+
+
+          <!--cta signup form end-->
+
+
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </section>
+
+
+  <!--welcome message end-->
+
+
+
+</header>
+
+
+
+<!--hero section end-->
+
+
+
+<!--How it works-->
+
+
+
+<section class="how-it-works section-spacing">
+
+  <div class="container">
+
+    <header class="section-header text-center">
+
+      <h2>Ok queremos te ajudar, agora entenda o que fazemos!</h2>
+
+      <h3>Somos um blog, sem fins lucrativos (Não vamos te vender nada, em nenhum momento). Então você deve se perguntar, o que ganhamos com isso? Veja abaixo o que nós ganhamos...</h3>
+
+    </header>
+
+    <div class="row">
+
+      <div class="col-md-11 center-block">
+
+
+
+        <!--step 1-->
+
+        <div class="row">
+
+          <div class="col-md-6 col-md-push-6 text-center"> <img src="img/step-1.svg" alt="step-1"> </div>
+
+          <div class="col-md-6 col-md-pull-6">
+
+            <div class="step-number"><span>1</span></div>
+
+            <h4>Ajudamos Pessoas </h4>
+
+            <p>Nós acreditamos que a internet é para todos, ou seja, para você! <br><br>
+            Com isso conseguimos fornecer informações que já existem na internet, porém outros blogs cobram por isso, nós não.
+            Vender pela internet é fácil e não precisa de investimento!</p>
+
+          </div>
+
+        </div>
+
+        <!--step 1 end-->
+
+
+
+        <!--step 2-->
+
+        <div class="row">
+
+          <div class="col-md-6 text-center"> <img src="img/step-2.svg" alt="step-2"> </div>
+
+          <div class="col-md-6">
+
+            <div class="step-number"><span>2</span></div>
+
+            <h4>Dicas Valiosas para você mesmo fazer!</h4>
+
+            <p>Nossa idéia é que para o inicio da sua experiencia, você não precise gastar, nem contratar ninguém, ou seja,
+            menos custos e menos riscos. Aliás, você já ouviu falar em Marketplace? É uma opção :)</p>
+
+          </div>
+
+        </div>
+
+        <!--step 2 end-->
+
+
+
+        <!--step 3-->
+
+        <div class="row">
+
+          <div class="col-md-6 col-md-push-6 text-center"> <img src="img/step-3.svg" alt="step-3"> </div>
+
+          <div class="col-md-6 col-md-pull-6">
+
+            <div class="step-number"><span>3</span></div>
+
+            <h4>Damos dicas de como apresentar seu produto!</h4>
+
+            <p>Vender na internet muitas vezes é saber oferecer da melhor maneira possível, seja por foto, descrição ou plataforma, então vamos lhe dar dicas para fazer isso da melhor maneira possível!</p>
+
+          </div>
+
+        </div>
+
+        <!--step 3 end-->
+
+
+
+        <!--step 4-->
+
+        <div class="row">
+
+          <div class="col-md-6 text-center"> <img src="img/step-4.svg" alt="step-4"> </div>
+
+          <div class="col-md-6">
+
+            <div class="step-number"><span>4</span></div>
+
+            <h4>Parceiros (O mais importante)</h4>
+
+            <p>Pense que você está sozinho no deserto! Pensou?? Então quem vai ver seu produto?<br><br>Vamos lhe ensinar de cara, a conseguir exibir seu produto para mais de 1 milhão de pessoas (DE GRAÇA)</p>
+
+          </div>
+
+        </div>
+
+        <!--step 4 end-->
+
+
+
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
+
+
+
+<!--How it works end-->
+
+
+
+
+
+
+<!--Benefits cta-->
+
+
+
+<!--Benefits cta end-->
+
+
+
+<!--User reviews-->
+
+
+
+<div class="user-reviews section-spacing text-center" id="reviews">
+
+  <div class="container">
+
+
+    <div class="row">
+
+      <div class="col-md-9 center-block">
+
+        <div class="reviews-slider owl-carousel owl-theme">
+
+          <div class="item">
+
+
+
+            <!--review 1-->
+
+            <blockquote>
+
+              <figure> <img src="img/user-review-1.jpg" alt="user" class="img-circle"> </figure>
+
+              <p>“Vivamus fringilla sem varius leo tempor, sed posuere nulla dapibus. Integer dapibus tortor quis libero ullamcorper rutrum neque ullamcorper convallis metus.”</p>
+
+              <footer> <cite>Willie Meyer, <i>Manager Facelook inc</i></cite> </footer>
+
+            </blockquote>
+
+            <!--review 1 end-->
+
+
+
+          </div>
+
+          <div class="item">
+
+
+
+            <!--review 2-->
+
+            <blockquote>
+
+              <figure> <img src="img/user-review-2.jpg" alt="user" class="img-circle"> </figure>
+
+              <p>“Nullam pellentesque nisl nec velit tincidunt viverra. Ut ut felis mi. Mauris tristique ante non feugiat fermentum morbi nec ultricies est accumsan ligula.”</p>
+
+              <footer> <cite>Jessica Campbell, <i>Blogger</i></cite> </footer>
+
+            </blockquote>
+
+            <!--review 2 end-->
+
+
+
+          </div>
+
+          <div class="item">
+
+
+
+            <!--review 3-->
+
+            <blockquote>
+
+              <figure> <img src="img/user-review-3.jpg" alt="user" class="img-circle"> </figure>
+
+              <p>“Donec ullamcorper imperdiet velit, et tincidunt lectus bibendum ut. Fusce ut nunc tortor. Donec vel tempus nisl, sed porta efficitur eu justo leo sollicitudin gravida.”</p>
+
+              <footer> <cite>Hannah Romero, <i>Designer at xyz</i></cite> </footer>
+
+            </blockquote>
+
+            <!--review 3 end-->
+
+
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+
+
+<!--User reviews end-->
+
+
+
+<!--learn more-->
+
+
+
+<div class="learn-more section-spacing">
+
+  <div class="container"><h1 style="text-align:center;">Confira alguns posts do nosso blog <br><br></h1>
+
+
+    <div class="row">
+
+      <div class="col-md-6 col-md-push-6">
+
+        <aside class="text-center">
+
+          <figure> <img src="img/learn-more-1.jpg" alt="learn more"> </figure>
+
+        </aside>
+
+      </div>
+
+      <div class="col-md-6 col-md-pull-6">
+
+        <article>
+
+          <h2><span>Post 1</span>Venda na Internet de maneira descomplicada!</h2>
+
+          <p>Entenda o conceito de Marketplace e descubra um novo jeito de oferecer seus produtos e serviços.
+  </p>
+
+          <a href="" class="learn-more-link">Leia Mais<i class="fa fa-angle-right"></i></a> </article>
+
+      </div>
+
+    </div>
+
+    <div class="row">
+
+      <div class="col-md-6">
+
+        <aside class="text-center">
+
+          <figure> <img src="img/learn-more-2.jpg" alt="learn more"> </figure>
+
+        </aside>
+
+      </div>
+
+      <div class="col-md-6">
+
+        <article>
+
+          <h2><span>Post 2</span> Cinco dicas para vender (bem) seu produto pela internet</h2>
+
+          <p>Muito se fala sobre a facilidade que a internet proporciona aos novos negócios, mas para que a empresa cumpra seu propósito e se destaque...</p>
+
+          <a href="" class="learn-more-link">Leia mais...<i class="fa fa-angle-right"></i></a> </article>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+
+
+<!--learn more end-->
+
+
+
+<!--Pricing-->
+
+
+<!--Pricing end-->
+
+
+
+<!--FAQ-->
+
+
+
+<!--FAQ end-->
+
+
+
+<!--Trusted clients-->
+
+
+
+
+<!--trusted clients end-->
+
+
+
+<!--About us -->
+
+<!--About us end-->
+
+
+
+<!--CTA footer-->
+
+
+
+<section class="cta-footer section-spacing text-center">
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-md-12">
+
+        <h2>Acesse agora e <em>de graça</em> para vender.<a  data-toggle="modal" data-target="#modal-contact-form" class="btn btn-secondary">Acessar</a></h2>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
+
+
+
+<!--CTA footer end-->
+
+
+
+<!--Twitter reviews-->
+
+
+
+
+<!--Twitter reviews end-->
+
+
+
+
+<!--Copyright terms-->
+
+
+
+<footer class="copyright-terms">
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-sm-5 col-md-6"> <small> &copy; 2016 Vamos te Ajudar. Todos os Direitos Reservados.</small> </div>
+
+      <div class="col-sm-7 col-md-6">
+
+
+      </div>
+
+    </div>
+
+  </div>
+
+</footer>
+
+
+
+<!--Copyright terms end-->
+
+
+
+<!--contact form modal-->
+
+
+
+<div class="modal fade" id="modal-contact-form" tabindex="-1" role="dialog">
+
+  <div class="modal-dialog" role="document">
+
+    <div class="modal-content">
+
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+
+      <div class="modal-body">
+
+
+
+        <!--contact form-->
+
+
+
+        <div class="contact-form text-center">
+
+          <header class="section-header"> <img src="img/support-icon.svg" alt="support icon">
+
+            <h2>Para acessar nosso conteúdo preencha abaixo</h2>
+
+            <h3>+ de 20 Posts sobre como vender na internet</h3>
+
+          </header>
+
+          <form class="cta-form cta-light" id="cadastro" name="cadastro" method="post" action="cadastro.php">
+
+            <div class="form-group">
+
+              <input  type="text" name="nome" class="contact-name form-control input-lg" placeholder="Seu Nome *" id="contact-name">
+
+            </div>
+
+            <div class="form-group">
+
+              <input  type="text"  name="email" class="contact-email form-control input-lg" placeholder="Seu E-mail *" id="contact-email">
+
+            </div>
+
+
+            <button type="submit" class="btn">Acessar conteúdo</button>
+
+          </form>
+
+        </div>
+
+
+
+        <!--contact form end-->
+
+
+
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+
+
+<!--contact form modal end-->
+
+
+
+<script src="js/jquery-2.2.1.min.js"></script>
+
+<script src="js/bootstrap.min.js"></script>
+
+<script src="js/jquery.magnific-popup.min.js"></script>
+
+<script src="js/owl.carousel.min.js"></script>
+
+<script src="js/jquery.waypoints.min.js"></script>
+
+<script src="js/jquery.animateNumber.min.js"></script>
+<!--
+<script src="js/jquery.ajaxchimp.min.js"></script> -->
+
+<script src="js/tweetie.min.js"></script>
+
+<!--[if IE 9]>
+
+<script src="js/placeholders.min.js"></script>
+
+<![endif]-->
+
+<script src="js/main.js"></script>
+
+<script async src="http://platform.twitter.com/widgets.js"></script>
+
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+
+<script src="js/gmap.js"></script>
+
+<script src="js/retina.min.js"></script>
+
+</body>
+
+</html>
